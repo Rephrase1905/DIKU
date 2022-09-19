3132d05eae651ebabe17b3351f5f67cd29f657cd
Mikail Oruc <flk369@alumni.ku.dk>
2022-09-19 15:55:03 +0200

    Update base5.fsx

--- DMA/base5.fsx
+++ DMA/base5.fsx
@@ -1,7 +1,10 @@
 //base 5 expansion
+printfn "Enter a number to be expanded in base 5"
+let c : int = int (System.Console.ReadLine())
+printfn "base 5 expansion of %A" c
 let rec base5 n =
     if n < 5 then
         [n]
     else
         (base5 (n / 5)) @ [n % 5]
-printfn "%A" (base5 158)
+printfn "%A" (base5 c)
