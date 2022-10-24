26203344120432dbdf859d4ec506d0c084dc5570
Mikail Oruc <flk369@alumni.ku.dk>
2022-10-25 00:31:34 +0200

    6g project

---
+++ PoP/6g/library.fs
@@ -0,0 +1,26 @@
+module library
+type pos = int * int
+type value = Red | Green | Blue | Yellow | Black //piece values
+type piece = value*pos
+type state = piece list
+
+let fromValue (v : value) : Canvas.color =
+    match v with
+    | Red -> { r = 255uy; g = 0uy; b = 0uy; a = 255uy }
+    | Green -> { r = 0uy; g = 255uy; b = 0uy; a = 255uy }
+    | Blue -> { r = 0uy; g = 0uy; b = 255uy; a = 255uy }
+    | Yellow -> { r = 255uy; g = 255uy; b = 0uy; a = 255uy }
+    | Black -> { r = 0uy; g = 0uy; b = 0uy; a = 255uy }
+
+let nextColor (c : value) : value =
+    match c with
+    | Red -> Green
+    | Green -> Blue
+    | Blue -> Yellow
+    | Yellow -> Black
+    | Black -> Black
+
+//let filter (k: int) (s : state) : state =
+
+let shiftUp (s : state) : state = 
+    
---
+++ PoP/6g/library.fsi
@@ -0,0 +1,14 @@
+module library
+type pos = int * int
+type value = Red | Green | Blue | Yellow | Black //piece values
+type piece = value*pos
+type state = piece list
+
+val fromValue : v : value -> Canvas.color
+val nextColor : c : value -> value
+val filter : k : int -> s : state -> state
+val shiftUp : s : state -> state
+val flipUD : s : state -> state
+val transpose : s : state -> state
+val empty : s : state -> pos list
+val addRandom : c : value -> s : state -> state option
