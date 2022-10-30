967824a6987c15d3ca5251d89c9310c24ffa5851
Mikail Oruc <111694420+Rephrase1905@users.noreply.github.com>
2022-10-30 18:43:21 +0100

    Added functions to be created

--- PoP/6g/library.fs
+++ PoP/6g/library.fs
@@ -3,7 +3,8 @@ type pos = int * int
 type value = Red | Green | Blue | Yellow | Black //piece values
 type piece = value*pos
 type state = piece list
-type color = {r:byte;g:byte;b:byte;a:byte}
+
+//functions for the game 2048
 
 let fromValue (v : value) : Canvas.color =
     match v with
@@ -21,7 +22,19 @@ let nextColor (c : value) : value =
     | Yellow -> Black
     | Black -> Black
 
-//let filter (k: int) (s : state) : state =
-
+//return the list of pieces on a column k on board
+let filter (k: int) (s : state) : state =
+    List.filter (fun (v: value, (x: int, y: int)) -> x = k) s
+// tilt all pieces on the board s to the left ( towards zero on
+// the first coordinate ), e.g. ,
+// > shiftUp [( Blue , (1 , 0)); (Red , (2 , 0)); (Black , (1 ,1))];;
+// val it: state = [( Blue , (0 , 0)); (Red , (1 , 0)); (Black , (0 , 1))]
 let shiftUp (s : state) : state = 
-    
+
+let flipUD (s : state) : state =
+
+let transpose (s : state) : state =
+
+let empty (s : state) : pos list =
+
+let addRandom (c : value) (s : state) : state option =
